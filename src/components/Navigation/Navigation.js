473f411e775e948d74df8f8b6aa1fd5c5a19e4e5
Suhag Al Amin <developersuhag@gmail.com>
2022-01-11 18:17:04 +0600

    initial commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,43 @@
+import { Container } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import * as React from "react";
+import logo from "../../images/logo.png";
+import user from "../../images/user.png";
+
+export default function Navigation() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{
+          bgcolor: "transparent",
+          color: "#222",
+          borderBottom: "3px solid #800080",
+          boxShadow: 0,
+        }}
+      >
+        <Toolbar>
+          <Container>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Box>
+                <img src={logo} alt="" />
+              </Box>
+              <Box>
+                <img src={user} alt="" />
+              </Box>
+            </Box>
+          </Container>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
